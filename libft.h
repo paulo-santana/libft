e334965c12e0124d8f97bbb7852c7f1be50910d5
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-13 23:25:56 -0300

    feat: add libft.h

---
+++ libft.h
@@ -0,0 +1,9 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+
+unsigned int	ft_strlen(char *str);
+void			*ft_memset(void *s, int c, long unsigned int n);
+void			ft_bzero(void *s, size_t n);
+
+#endif
