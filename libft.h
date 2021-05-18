940d72edb39e156aef38c54ba0187bb53c977513
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-17 22:11:08 -0300

    test broken code

--- libft.h
+++ libft.h
@@ -1,6 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psergio- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/04 19:38:55 by psergio-          #+#    #+#             */
+/*   Updated: 2021/04/05 04:11:20 by psergio-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
-# include <stddef.h>
 # define LIBFT_H
+
+# include <stddef.h>
 
 size_t	ft_strlen(char *str);
 void	*ft_memset(void *s, int c, long unsigned int n);
