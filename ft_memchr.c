877a624836f0ec707710d47098f5a154bb28b9e5
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-21 02:14:32 -0300

    style: dando um trato na norminha

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psergio- <psergio-@students.42sp.org.br>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/21 02:13:35 by psergio-          #+#    #+#             */
+/*   Updated: 2021/05/21 02:13:35 by psergio-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memchr(const void *area, int chr, size_t n)
@@ -8,10 +20,9 @@ void	*ft_memchr(const void *area, int chr, size_t n)
 	s = (unsigned char *)area;
 	if (!area)
 		return (NULL);
-	
 	i = 0;
 	while (i < n)
 		if (s[i++] == chr)
-			return (void *)(s + i - 1);
+			return ((void *)(s + i - 1));
 	return (NULL);
 }
