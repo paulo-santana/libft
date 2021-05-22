681f5646c6329ab58280df16a1020d56fa1628ed
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-21 22:31:45 -0300

    feat: finish part 1

--- libft.h
+++ libft.h
@@ -29,5 +29,14 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_isspace(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
