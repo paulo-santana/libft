22526807f673093db085fa95885b46c73b1adeef
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-23 00:48:47 -0300

    fix: make strlen's parameter contant

--- libft.h
+++ libft.h
@@ -22,7 +22,7 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-size_t	ft_strlen(char *str);
+size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, unsigned int size);
 char	*ft_strchr(const char *s, int c);
