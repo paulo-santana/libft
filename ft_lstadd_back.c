7ec882d18271824fcc8fd1e39b8b5de3cfdf9942
Paulo Sergio ramos de santana <psergio-@c1r2p1.42sp.org.br>
2021-05-27 15:52:22 -0300

    fix(lstadd_back): make **lst point to the right element

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: psergio- <psergio-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 14:24:15 by psergio-          #+#    #+#             */
-/*   Updated: 2021/05/27 14:24:15 by psergio-         ###   ########.fr       */
+/*   Updated: 2021/05/27 15:47:49 by psergio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,4 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		last->next = new;
 	else
 		*lst = new;
-}
-
-int	main(void)
-{
-	t_list	*l;
-	t_list	*n;
-
-	l = ((void *)0);
-	n = ft_lstnew(ft_strdup("OK"));
-	ft_lstadd_back(&l, n);
-	if (l == n && !strcmp(l->content, "OK"))
-	{
-		free(l->next);
-		free(l);
-		exit(TEST_SUCCESS);
-	}
-	free(l->next);
-	free(l);
-	exit(TEST_FAILED);
 }
