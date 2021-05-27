4f69acb334d75904d90fbc52132c741b69aae78b
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-27 13:42:25 -0300

    feat(list): add function to retrieve the last element

--- libft.h
+++ libft.h
@@ -62,5 +62,6 @@ void	ft_putnbr_fd(int nb, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
