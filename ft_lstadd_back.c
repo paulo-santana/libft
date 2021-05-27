bac4b672b0c7c020e7f6a45967a3ce18bb35abc0
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-27 15:44:41 -0300

    fix(listadd_back): make **lst the first element if it is empty

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -11,11 +11,38 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
+#define TEST_FAILED 1
+#define TEST_SUCCESS 0
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
+	if (!lst)
+		return ;
 	last = ft_lstlast(*lst);
-	last->next = new;
+	if (last)
+		last->next = new;
+	else
+		*lst = new;
+}
+
+int	main(void)
+{
+	t_list	*l;
+	t_list	*n;
+
+	l = ((void *)0);
+	n = ft_lstnew(ft_strdup("OK"));
+	ft_lstadd_back(&l, n);
+	if (l == n && !strcmp(l->content, "OK"))
+	{
+		free(l->next);
+		free(l);
+		exit(TEST_SUCCESS);
+	}
+	free(l->next);
+	free(l);
+	exit(TEST_FAILED);
 }
