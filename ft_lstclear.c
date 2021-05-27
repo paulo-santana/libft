322a7a45e60b0a0b36a61628182dbf581cdc14cf
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-27 16:27:12 -0300

    feat(list): add function to clear the list

---
+++ ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psergio- <psergio-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/27 16:08:40 by psergio-          #+#    #+#             */
+/*   Updated: 2021/05/27 16:08:40 by psergio-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*lsttmp;
+
+	if (!lst)
+		return ;
+	lsttmp = *lst;
+	while (*lst)
+	{
+		ft_lstdelone(lsttmp, del);
+		free(lsttmp);
+		*lst = (*lst)->next;
+	}
+	*lst = NULL;
+}
