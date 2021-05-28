c7dca05407fc2fd564141318cced6f4834229d50
Paulo Santana <paulo.santana.r@gmail.com>
2021-05-28 20:51:04 -0300

    refactor(lstclear): use delone to delete current node

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -19,8 +19,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	if (*lst)
 	{
 		ft_lstclear(&(*lst)->next, del);
-		del((*lst)->content);
-		free(*lst);
+		ft_lstdelone(*lst, del);
 		*lst = NULL;
 	}
 }
