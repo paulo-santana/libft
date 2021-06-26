17b9ac83884d95fe76a4c50fc57277c08574ff3b
Paulo Santana <paulo.santana.r@gmail.com>
2021-06-26 01:53:40 -0300

    fix: add ft_uitoa signature to header

--- libft.h
+++ libft.h
@@ -54,6 +54,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_itox(ssize_t n);
+char	*ft_uitoa(unsigned int nbr);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
